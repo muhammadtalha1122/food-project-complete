6495d6f7a47de00b1304a86a7f9be156c9daf6e6
muhammad talha <mt9898463@gmail.com>
2024-03-26 13:24:23 -0700

    new file

---
+++ config-file/firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAiSjtNty4yYGAoWEErRMsQO8uXCtaIWjM",
+
+    authDomain: "food-hunter-6e960.firebaseapp.com",
+
+    projectId: "food-hunter-6e960",
+
+    storageBucket: "food-hunter-6e960.appspot.com",
+
+    messagingSenderId: "551975360003",
+
+    appId: "1:551975360003:web:97486a201ce3c2fa8c2036"
+
+};
